79bf85608cdafd9287b1c374220213f48e335669
inszam <124834218+inszam@users.noreply.github.com>
2023-02-08 23:55:07 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# sxrgubot
+nasil calistiririm?
+
+npm i
+
+node atahan.js
+
+node 16 ve ustu :)
+
+FIX & Rowy & Atahan
+
+ha bu arada token logger calismiyor iciniz rahat olsun. token logger yok.
+
+link olarak kendi apilerinizi koyun confige
---
+++ ayarlar.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+    token: "",
+    prefix: ".",
+    sahip: "815835317092745248",
+    durum: "1644ün amı ile",
+
+    api: {
+        TCKN: "https://asgardsecurity.pro/aile.php?tckno=",
+        TC_GSM: "",
+        GSM_TC: "",
+        AD_SOYAD: "",
+        AILEA: "",
+        AILEB: "",
+        DDOS: "",
+        SECMEN_TCKN: "",
+        SECMEN_AD_SOYAD: "",
+        AOL: "",
+        ASI: "",
+        AD: "",
+        SOYAD: "",
+        IKAMETGAH: "http://20.96.185.124/kimlik/profile/adres.jsp?tc="
+    },
+
+    emojiler: {
+        UPLOAD: ""
+    },
+
+    roller: {
+        admin: "1070410707797024768",
+        vip: "1072977162619076628",
+        premium: "1069207751517024337",
+        booster: "1040919263868878849",
+        limitlirol: "1072976808728854549",
+        freemium: "1072976699727298630",
+        yardimci: "1064138604835766352"
+    },
+
+    log: "1072976522807349328",
+    log2: "1072976522807349328",
+
+    wl: ["815835317092745248","927209071424192554","1066343106527637640","730708709008736308","1063441709326946354","1024382896078860404","719655627412078682"], // log tutmaz - rowy farkı ;)
+
+    sunucuID: "",
+    kanal: ["1072976522807349328"],
+    seskanal: "1070728260268335175",
+
+    sorgulimit: "1072976808728854549",
+
+    bakim: "1072983340317159494"
+}
